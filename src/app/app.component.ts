a83bd48627aa804aa9ac04990cee795f3513a1ad
yukimir <luodadi.huazhu@gmail.com>
2018-10-15 23:09:59 +0900

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { saveAs } from 'file-saver';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,98 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'aigis-han';
+  constructor(private snackBar: MatSnackBar) {
+
+  }
+  textList = [];
+  displayedColumns = ['no', 'origin', 'trans'];
+  file: File = undefined;
+  onFileSelect(event) {
+    console.log(event);
+  }
+  @HostListener('document:dragleave', ['$event'])
+  @HostListener('document:dragenter', ['$event'])
+  @HostListener('document:dragover', ['$event'])
+  @HostListener('document:drop', ['$event'])
+  onDrage(e) {
+    e.preventDefault();
+  }
+  onFileDrop(e: DragEvent) {
+    const files = e.dataTransfer.files;
+
+    if (files.length !== 1) {
+      this.snackBar.open('暂不支持多文件', '', { verticalPosition: 'top' }); return;
+    }
+    const file = files[0];
+    if (file.type !== 'text/plain') {
+      this.snackBar.open('这不是TXT谢谢', '', { verticalPosition: 'top' }); return;
+    }
+
+    const reader = new FileReader();
+
+    const self = this;
+
+    const saved = localStorage.getItem(file.name);
+    if (saved) {
+      try {
+        const savedO = JSON.parse(saved);
+        if (savedO.lastModified > file.lastModified) {
+          this.textList = savedO.data;
+          self.file = file;
+          return;
+        }
+      } catch (ex) { console.log(ex); }
+    }
+
+    reader.onload = function (f) {
+      const r = self.readFile(this.result);
+      self.textList = r;
+      self.file = file;
+    };
+    reader.readAsText(file);
+  }
+  readFile(text: string) {
+    const lines = text.split('\r\n');
+    const data = [];
+    for (let i = 0; i < lines.length; i++) {
+      const ts = lines[i];
+      if (ts === '') { continue; }
+      const tsA = ts.split('\t');
+      tsA[0] = tsA[0].replace(/\\n/g, '<br>');
+      if (tsA[1]) {
+        tsA[1] = tsA[1].replace(/\\n/g, '\n');
+      }
+      const d = [i + 1, tsA[0] || '', tsA[1] || ''];
+      data.push(d);
+    }
+    return data;
+  }
+  onTextInput() {
+    this.saveText();
+  }
+  onKeyDown(event: KeyboardEvent, index) {
+    if (event.ctrlKey === true && event.code === 'Enter') {
+      const next = document.getElementById(`textarea-${index + 1}`);
+      if (next) {
+        next.focus();
+      }
+    }
+  }
+  saveText() {
+    localStorage.setItem(this.file.name, JSON.stringify({
+      lastModified: (new Date()).getTime(),
+      data: this.textList
+    }));
+  }
+  genText() {
+    let s = '';
+    for (const t of this.textList) {
+      s += `${t[1].replace(/<br>/g, '\\n')}\t${t[2].replace(/\n/g, '\\n')}\r\n`;
+    }
+    return s;
+  }
+  download() {
+    const blob = new Blob([this.genText()], { type: 'text/plain;charset=utf-8' });
+    saveAs(blob, this.file.name);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule, MatTableModule, MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
